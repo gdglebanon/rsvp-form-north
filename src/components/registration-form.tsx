6868240618cc8b3e7985a1f81b5cbd837b2994a4
gdglebanon <105236002+gdglebanon@users.noreply.github.com>
2025-11-18 20:05:31 +0100

    Update registration-form.tsx

--- src/components/registration-form.tsx
+++ src/components/registration-form.tsx
@@ -196,7 +196,7 @@ export default function RegistrationForm() {
     });
 
     try {
-      const response = await fetch('https://script.google.com/macros/s/AKfycby2a8ccAEehcZBv-YFtOUYtNntYXitcmwJ4fQjAN7IxYC3agLFW36oDDtIFL0oyumAJqQ/exec'), {
+      const response = await fetch('https://script.google.com/macros/s/AKfycby2a8ccAEehcZBv-YFtOUYtNntYXitcmwJ4fQjAN7IxYC3agLFW36oDDtIFL0oyumAJqQ/exec', {
         method: 'POST',
         headers: {
           'Content-Type': 'text/plain',
